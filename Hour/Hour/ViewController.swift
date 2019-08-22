049a83e08824a05fbae61997047f0efc8a80562c
InfinityCode777 <ngc35ster@gmail.com>
2019-08-21 21:35:11 -0400

    Init 1st project Hour

---
+++ Hour/Hour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hour
+//
+//  Created by Jing Wang on 8/21/19.
+//  Copyright © 2019 Jackalope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
